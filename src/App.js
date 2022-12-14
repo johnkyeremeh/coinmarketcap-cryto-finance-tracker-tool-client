0cb89367a90174101bd7409ed8c47f9a389dc5e0
johnkyeremeh <john.a.kyeremeh@gmail.com>
2022-12-14 07:43:17 -0800

    Import Components and create async function to fetch backend information. Use useEffect to start similar to componentdidmount and created init data state variable to house api data

--- src/App.js
+++ src/App.js
@@ -4,8 +4,30 @@ import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
 import CryptoList from './components/CryptoList';
 import axios from 'axios'
+import cryptodata from './data';
 
 function App() {
+  const [data, setData] = useState([])
+  
+  async function start(){
+    try {
+      const response = await axios.get(`http://localhost:4000/api`);
+      console.log("response", response)
+      const data = await response.data.data;
+      console.log("data", data)
+      setData(data)
+      console.log("data", cryptodata)
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    start()
+  }, []);
+
+
+
   return (
     <div className="App">
       <Navbar />

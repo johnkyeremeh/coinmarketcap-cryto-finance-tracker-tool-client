7bebf78e3d7e7fe6e3c03322a0129af774a03f3d
johnkyeremeh <john.a.kyeremeh@gmail.com>
2022-12-14 07:54:13 -0800

    Create initial component and setup for Navbar imported from Material UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
 import React from "react";
+import {AppBar, Toolbar,CssBaseline, Typography, makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+
+
+}));
 
 function Navbar() {
-
-
-  return (
-    <div>
-        Navbar
-    </div>
+    const classes = useStyles();
+    
+    return (
+    <AppBar  position="static">
+      <CssBaseline />
+      <Toolbar>
+        <Typography variant="h4" >TD Bank
+             <a href="#" to="/">HOME</a>
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 export default Navbar;

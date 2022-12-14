80cc7e43a55d68c66450a11241accb6749c57314
johnkyeremeh <john.a.kyeremeh@gmail.com>
2022-12-14 08:41:01 -0800

    Add initial styles for Dashboard with makeStyles constructor from material UI

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,28 +3,45 @@ import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
- 
-
+    main: {
+        display: "flex",
+        justifyContent: "space-between"
+    },
+    information: {
+        justifyContent: "space-between",
+        padding: "1rem 2rem 1rem 2rem",
+        borderRadius: "1rem",
+        color: "black",
+        backgroundColor: "white",
+        alignItems: "center",
+        width: "100%"
+    }, 
+    primary: {
+        marginTop: "20px",
+        color: "grey",
+        padding: "0"
+    }
 });
 
 function Dashboard() {
-  return (
+    const classes = useStyles();
+    return (
     <>
-        <div className="main">
-            <div className="main-information">
-                <h1 className="main--name">Total Balance</h1>
+        <div className={classes.main}>
+            <div className={classes.information}>
+                <h1 className={classes.primary}>Total Balance</h1>
                 <h2>$64.7K/28.2 ETH</h2>
-                <h4 className="main--name">Investment</h4>
+                <h4 className={classes.primary}>Investment</h4>
                 <h4 >$6,121,321,321 USD</h4>
-                <h4 className="main--name">Cash</h4>
+                <h4 className={classes.primary}>Cash</h4>
                 <h4 >$9,121,321,321 USD</h4>
             </div>
-            <div className="main-information">
-                <h1 className="main--name">Risk</h1>
+            <div className={classes.information}>
+                <h1 className={classes.primary}>Risk</h1>
                 <h2>Moderate</h2>
             </div>
-            <div className="main-information">
-                <h1 className="main--name">Gas Fee</h1>
+            <div className={classes.information}>
+                <h1 className={classes.primary}>Gas Fee</h1>
                 <h2>Moderate</h2>
             </div>
         </div>

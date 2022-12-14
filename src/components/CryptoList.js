17889fd58b1eaea29e881994603bd9501a154679
johnkyeremeh <john.a.kyeremeh@gmail.com>
2022-12-14 08:08:09 -0800

    Re-add to toFixed() method to percent_change data to have 2 decimal places

--- src/components/CryptoList.js
+++ src/components/CryptoList.js
@@ -70,9 +70,9 @@ export default function CryptoList(props) {
               <TableCell align="right">{row.cmc_rank}</TableCell>
               <TableCell align="right">{row.name}</TableCell>
               <TableCell align="right">{row.quote.USD.price}</TableCell>
-              <TableCell align="right">{row.quote.USD.percent_change_1h}</TableCell>
-              <TableCell align="right">{row.quote.USD.percent_change_24h}</TableCell>
-              <TableCell  align="right">{row.quote.USD.percent_change_7d}</TableCell>
+              <TableCell align="right">{row.quote.USD.percent_change_1h.toFixed(2)}</TableCell>
+              <TableCell align="right">{row.quote.USD.percent_change_24h.toFixed(2)}</TableCell>
+              <TableCell  align="right">{row.quote.USD.percent_change_7d.toFixed(2)}</TableCell>
               <TableCell align="right">{row.quote.USD.market_cap}</TableCell>
               <TableCell align="right">{row.quote.USD.volume_24h}</TableCell>
               </ExpandableTableRow>

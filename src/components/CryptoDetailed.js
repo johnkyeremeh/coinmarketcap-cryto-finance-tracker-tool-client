dbca75b68db0caa95500f938985c0c13d431ae66
johnkyeremeh <john.a.kyeremeh@gmail.com>
2022-12-14 09:00:45 -0800

    Add to CrytoDetailed component and confirmed props from CryptoList are received, copied formatter contructor from CryptoList into CryptoDetailed and called Market Stats section

--- src/components/CryptoDetailed.js
+++ src/components/CryptoDetailed.js
@@ -1,10 +1,69 @@
 import React from "react";
+import {makeStyles } from "@material-ui/core";
+import Button from '@mui/material/Button';
 
-export default function CryptoDetailed() {
+const useStyles = makeStyles((theme) => ({
+    
+}));
+
+// custom function to format text
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
+
+export default function CryptoDetailed(props) {
+    const classes = useStyles();
 
   return (
     <div>
-        CryptoDetailed
+         <div>
+            <div>
+                <h1>
+                {props.row.name}
+                </h1>
+            </div>
+          
+          <h1 >
+            Market Stats
+          </h1>
+          <div>
+            <div>
+                <div >Market Cap</div>
+                <p>{formatter.format(props.row.quote.USD.market_cap)}</p>
+            </div>
+            
+            <div>
+                <div>Volume</div>
+                <p>{formatter.format(props.row.quote.USD.volume_24h)}</p>
+            </div>
+    
+            <div>
+                <div>circulating supply</div>
+                <p>{formatter.format(props.row.quote.USD.volume_24h)}</p>
+            </div>
+            
+            <div>
+                <div>Typical Hold Time</div>
+                <p>{formatter.format(props.row.quote.USD.volume_24h)}</p>
+            </div>
+            
+            <div>
+                <div>Volume</div>
+                <p>{formatter.format(props.row.quote.USD.volume_24h)}</p>
+            </div>
+            
+        </div>
+        </div>
+        <div>
+          <h1>
+          Ready to get started?
+          </h1>
+          <h1>
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.
+          </h1>
+          <Button variant="contained">Sign Up</Button>
+        </div>
     </div>
   );
 }

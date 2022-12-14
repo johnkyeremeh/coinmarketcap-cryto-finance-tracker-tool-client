f387f1d5822f0668b21566acb24dd64a2bc0b175
johnkyeremeh <john.a.kyeremeh@gmail.com>
2022-12-14 08:47:51 -0800

    Update Navbar component with styles for mekeStyles constructor

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,26 @@ import React from "react";
 import {AppBar, Toolbar,CssBaseline, Typography, makeStyles} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-
+    logo: {
+        cursor: "pointer",
+        fontSize: "15px"
+      },
+      link: {
+        textDecoration: "none",
+        fontWeight: "400",
+        lineHeight:" 16px",
+        textAlign: "center",
+        color: "white",
+        fontSize: "14px",
+        marginLeft: theme.spacing(20),
+        "&:hover": {
+          color: "yellow",
+          borderBottom: "1px solid white",
+        },
+      },
+      appBar: {
+        backgroundColor: "#1a5336"
+      }
 
 }));
 
@@ -10,11 +29,11 @@ function Navbar() {
     const classes = useStyles();
     
     return (
-    <AppBar  position="static">
+    <AppBar className={classes.appBar} position="static">
       <CssBaseline />
       <Toolbar>
-        <Typography variant="h4" >TD Bank
-             <a href="#" to="/">HOME</a>
+        <Typography variant="h4" className={classes.logo}>TD Bank
+             <a href="#" to="/" className={classes.link}>HOME</a>
         </Typography>
       </Toolbar>
     </AppBar>
